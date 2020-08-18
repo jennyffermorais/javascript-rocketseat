fde360ec9fd69681ad3b9bb09fea725a7159885f
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-08-18 18:06:12 -0300

    adicionar resolução do exercício 1 - desafio 3

---
+++ desafio 3/exercicio-1.js
@@ -0,0 +1,25 @@
+// *******************  EXERCÍCIO 1  *******************
+
+// Crie uma função que recebe a idade de um usuário e retorna uma Promise que depois de 2
+// segundos retornará se usuário é maior ou não que 18 anos. Se o usuário ter mais que 18 anos de
+// idade o resultado deve cair no .then, caso contrário, no .catch
+
+function checaIdade(idade) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (idade >= 18) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 2000);
+  });
+}
+
+checaIdade(20)
+  .then(function () {
+    console.log("Maior que 18");
+  })
+  .catch(function () {
+    console.log("Menor que 18");
+  });
